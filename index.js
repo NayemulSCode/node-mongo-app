160b8e30ba6e575cd6d599e27c9301766cab38f6
Nayemul <nayemulhasan2016@gmail.com>
2021-03-24 17:20:55 +0600

    post product in cosole

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const uri = "mongodb+srv://saheb:sahebmongo2021@cluster0.vq7w3.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 app.get('/', (req, res) =>{
-    res.send('i am created')
+    //res.send('i am created')
+    res.sendFile(__dirname + '/index.html');
+
 })
 
 client.connect(err => {
   const collection = client.db("organicdb").collection("products");
   // perform actions on the collection object
+  app.post('/addProduct',(req, res) =>{
+    const product = req.body;
+    console.log(product);
+  })
   console.log('db connected');
   client.close();
 });
